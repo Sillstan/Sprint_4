2128022d23f532758739e929651f3fab10a056e1
Evgenii <151394283+Sillstan@users.noreply.github.com>
2024-01-03 16:55:48 +0200

    Update MakeAnOrderTest.java
    Update

--- src/test/java/MakeAnOrderTest.java
+++ src/test/java/MakeAnOrderTest.java
@@ -11,18 +11,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
-@RunWith(Parameterized.class) // запуск с параметризацией
+@RunWith(Parameterized.class)
 public class MakeAnOrderTest {
-    WebDriver driver; // объявление драйвера
-    private final String name; // объявление значения имени
-    private final String surname; // объявление значения фамилии
-    private final String address; // объявление значения адреса
-    private final String station; // объявление значения станции
-    private final String phoneNumber; // объявление значения номера телефона
-    private final String date; // объявление значения даты
-    private final String rentalPeriodValue; // объявление значения срока аренды
-    private final String scooterColorValue; // объявление значения цвета
-    private final String comment; // объявление значения комментария
+    WebDriver driver;
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final String station;
+    private final String phoneNumber;
+    private final String date;
+    private final String rentalPeriodValue;
+    private final String scooterColorValue;
+    private final String comment;
     public MakeAnOrderTest(String name, String surname, String address, String station, String phoneNumber, String date, String rentalPeriodValue, String scooterColorValue, String comment) {
         this.name = name;
         this.surname = surname;
@@ -33,9 +33,9 @@ public class MakeAnOrderTest {
         this.rentalPeriodValue = rentalPeriodValue;
         this.scooterColorValue = scooterColorValue;
         this.comment = comment;
-    } // инициализация переменных
+    }
 
-    @Parameterized.Parameters // параметры для теста
+    @Parameterized.Parameters
     public static Object[][] getDataTest() {
         return new Object[][] {
                 {"Добрыня", "Никитич", "ул. Ленина, 165", "Лихоборы", "89533333333", "26.12.2023", "сутки", "серая безысходность", "Звонок не работает"},
@@ -47,48 +47,48 @@ public class MakeAnOrderTest {
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup(); // установка драйвера браузера хром
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        /*WebDriverManager.firefoxdriver().setup(); // установка драйвера браузера фаерфокс
+        /*WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();*/
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // ожидание при поиске локаторов
-        driver.get("https://qa-scooter.praktikum-services.ru/"); // открытие страницы в браузере
-        driver.manage().window().fullscreen(); // фуллскрин для браузера
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        driver.manage().window().fullscreen();
     }
 
     @Test
     public void makeAnOrderTestWithEditOrderButtonHeader() {
-        MainPageElements mainPage = new MainPageElements(driver); // создание экземпляра главной страницы
-        mainPage.clickAcceptCookiesButton(); // принимаем куки
-        mainPage.clickEditOrderButtonHeader(); // нажатие на верхнюю кнопку заказать
-        ForWhomFormElements forWhomForm = new ForWhomFormElements(driver); // создание экземпляра страницы для кого самокат
-        forWhomForm.fillInTheFieldsFoWhomForm(name, surname, address, station, phoneNumber); // заполнение полей формы для кого самокат
-        forWhomForm.clickNextButton(); // клик по кнопке далее
-        AboutRentFormElements aboutRentForm = new AboutRentFormElements(driver); // создание экземпляра страницы про аренду
-        aboutRentForm.fillInTheFieldsAboutRentForm(date, rentalPeriodValue, scooterColorValue, comment); // заполнение полей формы про аренду
-        aboutRentForm.clickEditOrderButtonFinal(); // клик по кнопке заказать
-        aboutRentForm.clickYesButton(); // клик по кнопке да
-        Assert.assertTrue("Подтверждение заказа не появилось", aboutRentForm.isAcceptOrderHeaderDisplayed()); // проверка появления окна с подверждением оформления заказа
+        MainPageElements mainPage = new MainPageElements(driver);
+        mainPage.clickAcceptCookiesButton();
+        mainPage.clickEditOrderButtonHeader();
+        ForWhomFormElements forWhomForm = new ForWhomFormElements(driver);
+        forWhomForm.fillInTheFieldsFoWhomForm(name, surname, address, station, phoneNumber);
+        forWhomForm.clickNextButton();
+        AboutRentFormElements aboutRentForm = new AboutRentFormElements(driver);
+        aboutRentForm.fillInTheFieldsAboutRentForm(date, rentalPeriodValue, scooterColorValue, comment);
+        aboutRentForm.clickEditOrderButtonFinal();
+        aboutRentForm.clickYesButton();
+        Assert.assertTrue("Подтверждение заказа не появилось", aboutRentForm.isAcceptOrderHeaderDisplayed());
     }
 
     @Test
     public void makeAnOrderTestWithEditOrderButtonDown() {
-        MainPageElements mainPage = new MainPageElements(driver); // создание экземпляра главной страницы
-        mainPage.clickAcceptCookiesButton(); // принимаем куки
-        mainPage.scrollAndClickEditOrderButtonDown(); // скролл и нажатие на нижнюю кнопку заказать
-        ForWhomFormElements forWhomForm = new ForWhomFormElements(driver); // создание экземпляра страницы для кого самокат
-        forWhomForm.fillInTheFieldsFoWhomForm(name, surname, address, station, phoneNumber); // заполнение полей формы для кого самокат
-        forWhomForm.clickNextButton(); // клик по кнопке далее
-        AboutRentFormElements aboutRentForm = new AboutRentFormElements(driver); // создание экземпляра страницы про аренду
-        aboutRentForm.fillInTheFieldsAboutRentForm(date, rentalPeriodValue, scooterColorValue, comment); // заполнение полей формы про аренду
-        aboutRentForm.clickEditOrderButtonFinal(); // клик по кнопке заказать
-        aboutRentForm.clickYesButton(); // клик по кнопке да
-        Assert.assertTrue("Подтверждение заказа не появилось", aboutRentForm.isAcceptOrderHeaderDisplayed()); // проверка появления окна с подверждением оформления заказа
+        MainPageElements mainPage = new MainPageElements(driver);
+        mainPage.clickAcceptCookiesButton();
+        mainPage.scrollAndClickEditOrderButtonDown();
+        ForWhomFormElements forWhomForm = new ForWhomFormElements(driver);
+        forWhomForm.fillInTheFieldsFoWhomForm(name, surname, address, station, phoneNumber);
+        forWhomForm.clickNextButton();
+        AboutRentFormElements aboutRentForm = new AboutRentFormElements(driver);
+        aboutRentForm.fillInTheFieldsAboutRentForm(date, rentalPeriodValue, scooterColorValue, comment);
+        aboutRentForm.clickEditOrderButtonFinal();
+        aboutRentForm.clickYesButton();
+        Assert.assertTrue("Подтверждение заказа не появилось", aboutRentForm.isAcceptOrderHeaderDisplayed());
     }
 
 
     @After
     public void tearDown() {
         driver.quit();
-    } // закрываем браузер
+    }
 }
