c407c7d3dba15223712b919d8e05f9a7b0651525
Evgenii <151394283+Sillstan@users.noreply.github.com>
2024-01-03 16:48:39 +0200

    Update AboutRentFormElements.java
    Update

--- src/main/java/AboutRentFormElements.java
+++ src/main/java/AboutRentFormElements.java
@@ -2,32 +2,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AboutRentFormElements {
-    WebDriver driver; // объявление драйвера
+    WebDriver driver;
     public AboutRentFormElements(WebDriver driver) {
         this.driver = driver;
-    } // инициализация драйвера
-    private final By whenToDeliverField = By.xpath(".//*[@placeholder='* Когда привезти самокат']"); // поле "Дата доставки"
-    private final By rentalPeriodField = By.className("Dropdown-arrow"); // поле "Срок аренды"
-    private final String rentalPeriod = ".//*[text()='%s']"; // xpath для выпадающего списка со сроком аренды
-    private final String scooterColor = ".//*[text()='%s']"; // xpath для цвета самоката
-    private final By commentField = By.xpath(".//*[@placeholder='Комментарий для курьера']"); // поле "Комментарий"
-    private final By editOrderButtonFinal = By.xpath(".//button[contains(@class, 'Button_Button__ra12g Button_Middle__1CSJM') and contains(text(), 'Заказать')]"); // кнопка "Заказать"
-    private final By yesButton = By.xpath(".//button[contains(@class, 'Button_Button__ra12g Button_Middle__1CSJM') and text()='Да']"); // кнопка "Да" во всплывающем окне "Хотите сделать заказ?"
-    private final By acceptOrderHeader = By.xpath(".//div[text()='Заказ оформлен']"); // заголовок во всплывающем окне "Заказ оформлен"
+    }
+    private final By whenToDeliverField = By.xpath(".//*[@placeholder='* Когда привезти самокат']");
+    private final By rentalPeriodField = By.className("Dropdown-arrow");
+    private final String rentalPeriod = ".//*[text()='%s']";
+    private final String scooterColor = ".//*[text()='%s']";
+    private final By commentField = By.xpath(".//*[@placeholder='Комментарий для курьера']");
+    private final By editOrderButtonFinal = By.xpath(".//button[contains(@class, 'Button_Button__ra12g Button_Middle__1CSJM') and contains(text(), 'Заказать')]");
+    private final By yesButton = By.xpath(".//button[contains(@class, 'Button_Button__ra12g Button_Middle__1CSJM') and text()='Да']");
+    private final By acceptOrderHeader = By.xpath(".//div[text()='Заказ оформлен']");
     public void fillInTheFieldsAboutRentForm(String date, String rentalPeriodValue, String scooterColorValue, String comment) {
         driver.findElement(whenToDeliverField).sendKeys(date);
         driver.findElement(rentalPeriodField).click();
         driver.findElement(By.xpath(String.format(rentalPeriod, rentalPeriodValue))).click();
         driver.findElement(By.xpath(String.format(scooterColor, scooterColorValue))).click();
         driver.findElement(commentField).sendKeys(comment);
-    } // заполнение полей формы данными
+    }
     public void clickEditOrderButtonFinal() {
         driver.findElement(editOrderButtonFinal).click();
-    } // клик по кнопке заказать
+    }
     public void clickYesButton() {
         driver.findElement(yesButton).click();
-    } // клик по кнопке да
+    }
     public boolean isAcceptOrderHeaderDisplayed() {
         return driver.findElement(acceptOrderHeader).isDisplayed();
-    } // появление окна заказ оформлен
+    }
 }
